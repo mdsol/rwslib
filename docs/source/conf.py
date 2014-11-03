64d334dc67c587d063ac306ef6b0b7d2ae2e42ef
Ian Sparks <isparks@mdsol.com>
2014-11-03 06:41:32 -0500

    Changes related to preparing for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ import rwslib
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc','sphinxcontrib.exceltable']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
