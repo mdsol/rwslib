996bcb6f182a5b2b81e6bf754ca19e162f00feb0
Ian Sparks <isparks@mdsol.com>
2014-11-03 12:24:08 -0500

    Updated copyright notice in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'rwslib'
-copyright = u'2013, Medidata Solutions Inc'
+copyright = u'2014, Medidata Solutions Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
