3e15c817b52b41559a7a75a0641355970e825976
Geoff Low <glow@mdsol.com>
2015-11-20 16:19:02 +0000

    Always pass a time

--- rwslib/rws_requests/__init__.py
+++ rwslib/rws_requests/__init__.py
@@ -35,7 +35,7 @@ def format_date_argument(date_element):
         if 'T' in date_element:
             _date = datetime.datetime.strptime(date_element, "%Y-%m-%dT%H:%M:%S")
         else:
-            _date = datetime.datetime.strptime(date_element, "%Y-%m-%d").date()
+            _date = datetime.datetime.strptime(date_element, "%Y-%m-%d")
     else:
         _date = date_element
     return _date.isoformat()
