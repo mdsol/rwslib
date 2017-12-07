d60b907b5f9d6c51d544162c06e5c859b17c7c89
Geoff Low <glow@mdsol.com>
2017-12-07 10:42:13 +0000

    Added URL metadata

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     install_requires=['requests', 'lxml', 'httpretty', 'six', 'click', 'faker', 'enum34'],
     tests_require=['mock'],
     license=open('LICENSE.txt').read(),
+    url='https://github.com/mdsol/rwslib/',
     zip_safe=False,
     test_suite='rwslib.tests.all_tests',
     classifiers=(
