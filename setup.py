39f01c9493a65be3a5a46f44cf3029da4d67744c
Geoff Low <glow@mdsol.com>
2018-11-09 09:36:21 +0000

    Reformat using Black Issue with Classifier tuple v List

--- setup.py
+++ setup.py
@@ -9,59 +9,60 @@ try:
 except ImportError:
     from distutils.core import setup
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     sys.exit()
 
 packages = [
-    'rwslib',
-    'rwslib.rws_requests',
-    'rwslib.builders',
-    'rwslib.extras',
-    'rwslib.extras.audit_event',
-    'rwslib.extras.rwscmd',
+    "rwslib",
+    "rwslib.rws_requests",
+    "rwslib.builders",
+    "rwslib.extras",
+    "rwslib.extras.audit_event",
+    "rwslib.extras.rwscmd",
 ]
 
-rwsinit = open('rwslib/__init__.py').read()
-author = re.search("__author__ = '([^']+)'", rwsinit).group(1)
-version = re.search("__version__ = '([^']+)'", rwsinit).group(1)
+rwsinit = open("rwslib/__init__.py").read()
+author = re.search(r"__author__ = \"([^']+)\"", rwsinit).group(1)
+version = re.search(r"__version__ = \"([^']+)\"", rwsinit).group(1)
 
 setup(
-    name='rwslib',
+    name="rwslib",
     version=version,
-    description='Rave web services for Python',
-    long_description=open('README.md').read(),
+    description="Rave web services for Python",
+    long_description=open("README.md").read(),
     author=author,
     author_email="isparks@mdsol.com",
     packages=packages,
-    package_dir={'rwslib': 'rwslib'},
+    package_dir={"rwslib": "rwslib"},
     include_package_data=True,
-    install_requires=('requests',
-                      'lxml',
-                      'httpretty',
-                      'six',
-                      'click',
-                      'faker',
-                      "enum34; python_version < '3.4'",
-                      ),
-    tests_require=['mock'],
-    license=open('LICENSE.txt').read(),
-    url='https://github.com/mdsol/rwslib/',
-    zip_safe=False,
-    test_suite='rwslib.tests.all_tests',
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: PyPy',
+    install_requires=(
+        "requests",
+        "lxml",
+        "httpretty",
+        "six",
+        "click",
+        "faker",
+        "enum34; python_version < '3.4'",
     ),
-    entry_points='''
+    tests_require=["mock"],
+    license=open("LICENSE.txt").read(),
+    url="https://github.com/mdsol/rwslib/",
+    zip_safe=False,
+    test_suite="rwslib.tests.all_tests",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    entry_points="""
     [console_scripts]
     rwscmd=rwslib.extras.rwscmd.rwscmd:rws
-    ''',
+    """,
 )
