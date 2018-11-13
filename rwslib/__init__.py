51f092e093359dc1ee3985032ee556e6da1252de
Geoff Low <glow@mdsol.com>
2018-11-13 11:40:25 +0000

    Remove double encoding

--- rwslib/__init__.py
+++ rwslib/__init__.py
@@ -124,8 +124,6 @@ class RWSConnection(object):
         self.request_time = time.time() - start_time
         self.last_result = r  # see also r.elapsed for timedelta object.
 
-        # ensure the response.text is usable
-        r.encoding = "utf-8-sig"
         if r.status_code in [400, 404]:
             # Is it a RWS response?
             if r.text.startswith("<Response"):
